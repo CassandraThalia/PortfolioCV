bedbb8a6e426edb5d75a2b8d6b0b8cacddfe35d9
CassandraThalia <W0449188@nscc.ca>
2021-03-09 11:38:06 -0400

    jquery working

--- portfolio_java.js
+++ portfolio_java.js
@@ -1,10 +1,25 @@
 
-function showElement(p_element) {
-    var element = document.getElementById(p_element);
-    if (element.style.display === "block"){
-        element.style.display = "none";
-    }
-    else {
-        element.style.display = "block";
-    }
-}
+// function showElement(p_element) {
+//     var element = document.getElementById(p_element);
+//     if (element.style.display === "block"){
+//         element.style.display = "none";
+//     }
+//     else {
+//         element.style.display = "block";
+//     }
+// }
+
+  $(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},1000);  
+            }
+        }); 
+    });
+});
