241ee912f7b3831da6281b2b4df516cdc2e13859
CassandraThalia <W0449188@nscc.ca>
2021-03-09 15:16:59 -0400

    basically finished! does not fade in on load

--- portfolio_java.js
+++ portfolio_java.js
@@ -9,7 +9,8 @@
 //     }
 // }
 
-  $(document).ready(function() {
+
+$(document).ready(function() {
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
         /* Check the location of each desired element */
@@ -23,3 +24,28 @@
         }); 
     });
 });
+
+
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).on('scroll', function(){
+        /* Check the location of each desired element */
+        $('.hidemeless').each( function(i){
+            var top_of_object = $(this).offset().top + 200;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > top_of_object ){
+                $(this).animate({'opacity':'1'},1000);  
+            }
+        }); 
+    });
+});
+
+$(document).ready(function(){
+    setTimeout(function() {
+    /*! Fades in page on load */
+    $('.mainCont').fadeIn(1500);
+    }, 1000);
+});
+
+
